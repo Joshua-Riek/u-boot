7484a814fbb80d0ab6f9f103029958a5cbca7835
Joshua-Riek <jjriek@verizon.net>
2023-10-04 18:45:54 -0400

    board: rockchip: add Turing RK1 RK3588

---
+++ include/configs/turing-rk1-rk3588.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Rockchip Electronics Co., Ltd.
+ */
+
+#ifndef __TURING_RK1_RK3588_H
+#define __TURING_RK1_RK3588_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3588_common.h>
+
+#ifdef BOOT_TARGETS
+#undef BOOT_TARGETS
+#endif
+
+#define BOOT_TARGETS	"mmc1 nvme scsi usb mmc0 pxe dhcp spi"
+
+#endif /* __TURING_RK1_RK3588_H */
